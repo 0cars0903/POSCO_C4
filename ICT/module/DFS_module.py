65c163580d7cbff82ce6947d848219ab1c7af6dd
0cars0903 <111993984+0cars0903@users.noreply.github.com>
2023-01-31 21:21:38 +0900

    Add files via upload

---
+++ ICT/module/DFS_module.py
@@ -0,0 +1,22 @@
+# 그래프의 형태 dictionary 형태로 저장)
+graph_list = {1: set([3, 4]),
+              2: set([3, 4, 5]),
+              3: set([1, 5]),
+              4: set([1]),
+              5: set([2, 6]),
+              6: set([3, 5])}
+
+root_node = 1 # 시작점
+
+def DFS_with_adj_list(graph, root):
+    visited = []
+    stack = [root]
+
+    while stack:
+        n = stack.pop()
+        if n not in visited:
+            visited.append(n)
+            stack += graph[n] - set(visited)
+    return visited
+
+print(DFS_with_adj_list(graph_list, root_node))
